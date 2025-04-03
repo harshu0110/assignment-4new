989b65d61a37aced20f22d72494ef31598197430
harshu0110 <harshika.g.v@gmail.com>
2025-04-03 18:31:34 -0400

    fifthcommit-part-2

--- part-2/main.js
+++ part-2/main.js
@@ -1,18 +1,10 @@
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
-
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
-
-/* Declaring the array of image filenames */
 const imageFiles = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg'];
-
-/* Declaring the alternative text for each image file */
-const altTexts = ['Closeup of a human eye','Wavy white and blue fabric',
-    'Purple and white flowers','Ancient Egyptian wall painting',
-    'Butterfly on a green leaf'];
-
-/* Looping through images */
+const altTexts = [
+    'Closeup of a human eye','Wavy white and blue fabric','Purple and white flowers','Ancient Egyptian wall painting','Butterfly on a green leaf'];
 imageFiles.forEach((src, index) => {
     const newImage = document.createElement('img');
     newImage.setAttribute('src', src);
@@ -23,10 +15,12 @@ imageFiles.forEach((src, index) => {
         displayedImage.alt = altTexts[index];
     });
 });
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
-
-/* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', () => {
+    if (btn.textContent === 'Darken') {
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+        btn.textContent = 'Lighten';
+    } else {
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+        btn.textContent = 'Darken';
+    }
+});
