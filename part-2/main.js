73adfeebecee11e7bedac20b172b71c43fe009ba
harshu0110 <harshika.g.v@gmail.com>
2025-04-03 17:59:57 -0400

    Thirdcommit-part-2

--- part-2/main.js
+++ part-2/main.js
@@ -5,8 +5,12 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageFiles = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const altTexts = ['Closeup of a human eye','Wavy white and blue fabric',
+    'Purple and white flowers','Ancient Egyptian wall painting',
+    'Butterfly on a green leaf'];
 
 /* Looping through images */
 
