9f2873625ccf276625a03093e03e633f2834fa2e
harshu0110 <harshika.g.v@gmail.com>
2025-04-03 14:00:18 -0400

    secondcommit-part-1

--- main.js
+++ main.js
@@ -1,29 +1,20 @@
-1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
-
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
-function randomValueFromArray(array){
-  const random = Math.floor(Math.random()*array.length);
-  return array[random];
+function randomValueFromArray(array) {
+  return array[Math.floor(Math.random() * array.length)];
 }
-
 2. RAW TEXT STRINGS
 
-It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
+const storyText =
+  "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
-Willy the Goblin
-Big Daddy
-Father Christmas
-
-the soup kitchen
-Disneyland
-the White House
-
-spontaneously combusted
-melted into a puddle on the sidewalk
-turned into a slug and crawled away
+const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const insertY = ["the soup kitchen", "Disneyland", "the White House"];
+const insertZ = ["spontaneously combusted",
+  "melted into a puddle on the sidewalk",
+  "turned into a slug and crawled away"];
 
 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
 
